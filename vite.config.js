afdf7370050af52445cada06c32dd41ab0f77189
Mithun Halder <mithunhaldar7500@gmail.com>
2026-02-19 14:08:57 +0530

    Fix: Add base path configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/the-crochet-land/',  // 👈 repo name yaha daalo
   plugins: [react()],  // 👈 repo name yaha daalo
 })
