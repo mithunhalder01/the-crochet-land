a4943cbbdbb7e4aae2eed0a7717c6b2181623b95
The Mithun Halder <mac@macs-MacBook-Pro.local>
2026-02-19 10:56:35 +0530

    Updated homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
 // tailwind.config.js
-module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        brand: {
-          primary: "#FFB1B1",   // Cute Pink
-          secondary: "#FFD5B4", // Soft Peach
-          bg: "#FFF9F0",        // Creamy Background
-          text: "#4A3434",      // Soft Brown
-        }
-      }
-    }
-  }
-}
-
 export default {
   content: [
     "./index.html",
